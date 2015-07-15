94879eb6e45f4e78ef26f93bd9f593b82355feef
Anthony Cantuta <anthony@cantuta.org>
2015-07-15 10:21:15 -0500

    Agrega datos de ejemplo

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,7 +77,7 @@ areas = [
 
 tipos_documento = ['Solicitud', 'Oficio', 'Carta']
 nombres = ['JUAN', 'MARIA', 'JOSÉ', 'GUSTAVO', 'LUIS', 'YOVANA','KATY', 'GABRIEL', 'FRANKLIN', 'VICTOR', 'DIEGO', 'FRANZ', 'RODRIGO']
-apellidos = ['PEREZ', 'CRUZ', 'MAMANI', 'QUISPE', 'ARPASI', 'REAÑO','JUAREZ', 'PAREDES', 'PARI', 'NINARAQUE', 'YUFRA', 'GUTIERREZ']
+apellidos = ['PEREZ', 'CRUZ', 'MAMANI', 'QUISPE', 'ARPASI', 'REAÑO','JUAREZ', 'PAREDES', 'PARI', 'QUISPE', 'YUFRA', 'GUTIERREZ', 'HUAMAN', 'CHAMBILLA']
 
 areas.each do |area|
   Area.create(nombre: area)
@@ -103,4 +103,95 @@ end
   ua.area = a
   ua.es_jefe = true
   ua.save
+end
+
+# Creando documentos
+count_doctipo = Doctipo.count
+count_user = User.count
+count_area = Area.count
+count_movaccion = Movaccion.count
+500.times do 
+
+  offset_doctipo = rand(count_doctipo)
+  offset_user_remitente = rand(1...count_user)
+  offset_user_destinatario = rand(1...count_user)
+  offset_area = rand(count_area)
+  offset_area_destino = rand(count_area)
+  offset_movaccion = rand(count_movaccion)
+
+  random_user_remitente = User.offset(offset_user_remitente).first
+  random_user_destinatario = User.offset(offset_user_destinatario).first
+  random_area = Area.offset(offset_area).first
+  random_area_destino = Area.offset(offset_area_destino).first
+
+  begin
+    offset_area_destino = rand(count_area)
+    random_area_destino = Area.offset(offset_area_destino).first
+  end while(random_area_destino.id == random_area.id)
+  
+  random_movaccion = Movaccion.offset(offset_movaccion).first
+
+  d = Documento.new
+  d.doctipo = Doctipo.offset(offset_doctipo).first
+  d.nro = "NRO #{rand(1..9999999999).to_s.rjust(10,'0')}"
+  d.asunto = "Asunto #{Faker::Lorem.sentence}"
+  d.user = random_user_remitente
+  d.remitente = random_user_remitente.fullname
+  d.cod_remitente = random_user_remitente.username
+  d.destinatario = random_user_destinatario.fullname
+  d.area_generadora_id = random_area.id
+  d.save!
+
+
+  Doclog.create(documento_id: d.id, contenido: "Se ha registrado documento.")
+
+  dm = Docmovimiento.new
+  dm.area_fuente = random_area
+  dm.area_destino = random_area_destino
+  dm.movaccion = random_movaccion
+  dm.documento = d
+  dm.recibido = true
+  dm.save
+
+  area_envia = random_area
+
+  documento_hijo = d
+  cant_doc = rand(1..15)
+  cant_doc.times do |i|
+    begin
+      offset_area_destino = rand(count_area)
+      random_area_destino = Area.offset(offset_area_destino).first
+    end while(random_area_destino.id == area_envia.id)
+
+    d = Documento.new
+    d.doctipo = Doctipo.offset(offset_doctipo).first
+    d.nro = "NRO #{rand(1..9999999999).to_s.rjust(10,'0')}"
+    d.asunto = "Asunto #{Faker::Lorem.sentence}"
+    d.user = random_user_remitente
+    d.remitente = random_user_remitente.fullname
+    d.cod_remitente = random_user_remitente.username
+    d.destinatario = random_user_destinatario.fullname
+    d.area_generadora_id = area_envia.id
+    d.documento_hijo = documento_hijo
+
+    dm = Docmovimiento.new
+    dm.area_fuente = area_envia
+    dm.area_destino = random_area_destino
+    dm.movaccion = random_movaccion
+    dm.documento = d
+
+    if(i == (cant_doc-1) )
+      dm.recibido = [true, false].sample
+      #dm.recibido = true
+    else
+      dm.recibido = true
+    end
+    
+    area_envia = dm.area_destino 
+    documento_hijo = d
+
+    dm.save!
+    
+    
+  end
 end
